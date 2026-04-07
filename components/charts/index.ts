7f9ac9139e868383f9eabbf78d2bfb350640b054
Bianca Gjokaj <bianca.gjokaj@gmail.com>
2026-04-07 16:59:31 -0400

    barrel export for charts

---
+++ components/charts/index.ts
@@ -0,0 +1,4 @@
+export { default as ScatterChart } from './ScatterChart';
+export { default as LineChart } from './LineChart';
+export { default as BarChart } from './BarChart';
+export { default as DoughnutChart } from './DoughnutChart';
