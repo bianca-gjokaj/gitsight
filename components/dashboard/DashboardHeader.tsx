411f407b7492d212b02aa3230fd89db01ef3e37a
Bianca Gjokaj <bianca.gjokaj@gmail.com>
2026-04-07 18:30:11 -0400

    Composed the sectionheader & filterpill

---
+++ components/dashboard/DashboardHeader.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { SectionHeader, FilterPill } from "../ui";
+import { REPO_FILTERS } from "@/lib/constants";
+import type { GitHubUser, RepoFilter } from "@/types";
+
+interface DashboardHeaderProps {
+  user: GitHubUser;
+  activeFilter: RepoFilter;
+  onFilterChange: (filter: RepoFilter) => void;
+}
+
+export default function DashboardHeader({
+  user, activeFilter, onFilterChange,
+}: DashboardHeaderProps) {
+  return (
+    <SectionHeader
+    title={`${user.name || user.login}\u2019s Analytics`}
+    subtitle={user.bio || undefined}
+    >
+      {REPO_FILTERS.map((filter) => (
+        <FilterPill
+        key={filter.value}
+        label={filter.label}
+        active={activeFilter === filter.value}
+        onClick={() => onFilterChange(filter.value as RepoFilter)}
+        />
+      ))}
+    </SectionHeader>
+  );
+}
